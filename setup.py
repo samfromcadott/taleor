34583be0b29e0583151375f9657e0d809c75766f
Sam Jackson <samahstan@gmail.com>
2013-10-01 16:19:11 -0500

    Changed 'packages' to 'py_modules'

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 
-setup(name = 'Tale-or',
+setup(name = 'taleor',
       version = 'Alpha',
       description = 'Interactive fiction Library',
-      author = 'Sam Jackson'
-      url='https://github.com/samgj/taleor',
-      packages=['tale-or'],
+      author = 'Sam Jackson',
+      url = 'https://github.com/samgj/taleor',
+      py_modules = ['tale-or']
      )
