3a5a1a8b96dfdf24d08cdb38f7f7b62606bbabee
Sam Jackson <samahstan@gmail.com>
2013-10-01 16:14:58 -0500

    Changed packages to py_modules

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = 'Tale-or',
+      version = 'Alpha',
+      description = 'Interactive fiction Library',
+      author = 'Sam Jackson'
+      url='https://github.com/samgj/taleor',
+      packages=['tale-or'],
+     )
